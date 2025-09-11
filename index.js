08be75626deb9d3bcb495f30048939ecb1d74ad8
mr-banner <sahukamalkant28@gmail.com>
2025-09-11 19:57:22 +0530

    updated origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5001
 const app = express();
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://afro-vids.vercel.app"],
   credentials: true
 }))
 app.use(cookieParser());
